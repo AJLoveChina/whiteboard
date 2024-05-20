9a96a8c49c068ed8c2123f4f4bc7237a386e4b6e
AJLoveChina <ajloveworld@gmail.com>
2024-05-19 19:19:15 -0700

    chore: Protractor

---
+++ src/common/data.ts
@@ -0,0 +1,14 @@
+export const TEMPLATES_LIST = [
+  {
+    img: process.env.PUBLIC_URL + "/imgs/world-map.png",
+    url: "https://zoom.us/wb/doc/QasS9btCQweutOjjaFV06Q",
+    title: "World Map",
+    desc: "A world map is a graphical representation of Earth's surface, illustrating continents, countries, bodies of water, and geographic features. It offers a comprehensive view of global geography, including political boundaries, landforms, and oceans. Varying in scale and detail, world maps aid in navigation, education, and research across disciplines like geography, history, and environmental science. They serve as essential tools for understanding spatial relationships, historical contexts, and global patterns.",
+  },
+  {
+    img: process.env.PUBLIC_URL + "/imgs/Protractor1.svg",
+    url: "https://zoom.us/wb/doc/KsZh7EqHRrqRb2kOd7UQ9w",
+    title: "Protractor",
+    desc: "In mathematics, a protractor is a tool used to measure angles. It typically consists of a semicircular plastic or metal tool with degree markings along its curved edge. Protractors are commonly used in geometry to measure and draw angles accurately. They allow students to visually understand the concept of angles and practice measuring them with precision.",
+  },
+];
--- src/components/Whiteboards.tsx
+++ src/components/Whiteboards.tsx
@@ -7,24 +7,16 @@ import {
   Button,
 } from "@mui/material";
 import style from "./common.module.css";
-
-const list = [
-  {
-    img: process.env.PUBLIC_URL + "/imgs/world-map.png",
-    url: "https://zoom.us/wb/doc/QasS9btCQweutOjjaFV06Q",
-    title: "World Map",
-    desc: "",
-  },
-];
+import { TEMPLATES_LIST } from "../common/data";
 
 export function Whiteboards() {
   return (
     <div className={style.wbs}>
-      {list.map((item) => {
+      {TEMPLATES_LIST.map((item) => {
         return (
-          <Card sx={{ maxWidth: 300 }} className={style.wb}>
+          <Card sx={{ maxWidth: 300 }} className={style.wb} key={item.img}>
             <CardMedia
-              sx={{ height: 140 }}
+              sx={{ height: 200 }}
               image={item.img}
               title={item.title}
             />
@@ -32,12 +24,16 @@ export function Whiteboards() {
               <Typography gutterBottom variant="h5" component="div">
                 {item.title}
               </Typography>
-              <Typography variant="body2" color="text.secondary">
+              <Typography
+                variant="body2"
+                color="text.secondary"
+                className={style.desc}
+              >
                 {item.desc}
               </Typography>
             </CardContent>
             <CardActions>
-              <Button size="small" href={item.url}>
+              <Button size="small" href={item.url} target="_blank">
                 Open
               </Button>
             </CardActions>
