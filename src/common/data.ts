8dc109cc2638bc21fb0f38993a80624722d336af
AJLoveChina <ajloveworld@gmail.com>
2024-05-19 20:58:52 -0700

    chore: International Chess

--- src/common/data.ts
+++ src/common/data.ts
@@ -11,4 +11,10 @@ export const TEMPLATES_LIST = [
     title: "Protractor",
     desc: "In mathematics, a protractor is a tool used to measure angles. It typically consists of a semicircular plastic or metal tool with degree markings along its curved edge. Protractors are commonly used in geometry to measure and draw angles accurately. They allow students to visually understand the concept of angles and practice measuring them with precision.",
   },
+  {
+    img: process.env.PUBLIC_URL + "/imgs/international-chess.png",
+    url: "https://zoom.us/wb/doc/-Hs1ctj2TiqgS2qadaOd4A",
+    title: "International Chess",
+    desc: "Playing International Chess on Zoom's whiteboard feature allows players to engage in strategic battles remotely. Participants draw and move pieces with digital tools, mimicking the traditional board experience. Communication is key as players discuss moves and plan strategies via Zoom's audio or chat functions. The virtual setting adds a modern twist, enabling friends, family, or competitors from around the world to compete in real-time. While lacking the tactile sensation of physical pieces, the convenience and accessibility of online play broaden the game's reach. Despite the distance, the thrill of outsmarting opponents and achieving victory remains as captivating as ever.",
+  },
 ];
