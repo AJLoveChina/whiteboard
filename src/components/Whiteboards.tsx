acc881b474cd863314ea115861458bdb97a3d2fc
AJLoveChina <ajloveworld@gmail.com>
2024-05-20 22:07:13 -0700

    chore: grid

--- src/components/Whiteboards.tsx
+++ src/components/Whiteboards.tsx
@@ -1,45 +1,48 @@
 import {
-  Card,
-  CardMedia,
-  CardContent,
-  Typography,
-  CardActions,
   Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
 } from "@mui/material";
-import style from "./common.module.css";
 import { TEMPLATES_LIST } from "../common/data";
+import style from "./common.module.css";
 
 export function Whiteboards() {
   return (
-    <div className={style.wbs}>
+    <Grid container className={style.wbs}>
       {TEMPLATES_LIST.map((item) => {
         return (
-          <Card sx={{ maxWidth: 300 }} className={style.wb} key={item.img}>
-            <CardMedia
-              sx={{ height: 200 }}
-              image={item.img}
-              title={item.title}
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                {item.title}
-              </Typography>
-              <Typography
-                variant="body2"
-                color="text.secondary"
-                className={style.desc}
-              >
-                {item.desc}
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small" href={item.url} target="_blank">
-                Open
-              </Button>
-            </CardActions>
-          </Card>
+          <Grid item lg={2} md={3} sm={4} xs={6}>
+            <Card sx={{ maxWidth: 300 }} className={style.wb} key={item.img}>
+              <CardMedia
+                sx={{ height: 200 }}
+                image={item.img}
+                title={item.title}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {item.title}
+                </Typography>
+                <Typography
+                  variant="body2"
+                  color="text.secondary"
+                  className={style.desc}
+                >
+                  {item.desc}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small" href={item.url} target="_blank">
+                  Open
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
         );
       })}
-    </div>
+    </Grid>
   );
 }
